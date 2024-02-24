c3c3773764f9a112913d556827a0c247ff0f5f27
Ulvi Spaethe <ulvispaethe@gmail.com>
2024-02-24 18:08:01 +0000

    created and run servern

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Thirteen</span>
         Blog
         </Link>
-        <form>
+        <form >
             <TextInput type='text' placeholder='Search ...' rightIcon={FaSearch} className='hidden lg:inline' />
         </form>
         <Button className='w-12 h-10 lg:hidden' color='gray' pill>
