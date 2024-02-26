2274345ad943f5a406c55851697ef7c6dc97b88f
Ulvi Spaethe <ulvispaethe@gmail.com>
2024-02-26 22:38:08 +0000

    updated header component with user data

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,53 +1,78 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import { FaMoon, FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-    const path = useLocation().pathname
-  return (
-    <Navbar className='border-b-2'>
-        <Link to={`/`} className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Thirteen</span>
-        Blog
-        </Link>
-        <Navbar.Collapse>
-                <Navbar.Link active={path === "/"} as={'div'}>
-                    <Link to={'/'}>
-                    Home
-                    </Link>
-                </Navbar.Link>
-                <Navbar.Link active={path === "/about"} as={'div'}>
-                    <Link to={'/about'}>
-                    About
-                    </Link>
-                </Navbar.Link>
-                <Navbar.Link active={path === "/projects"} as={'div'}>
-                    <Link to={'/projects'}>
-                    Projects
-                    </Link>
-                </Navbar.Link>
-            </Navbar.Collapse>
-        <form >
-            <TextInput type='text' placeholder='Search ...' rightIcon={FaSearch} className='hidden lg:inline' />
-        </form>
-        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-            <FaSearch/>
-        </Button>
-        <div className='flex gap-2 md:order-2'>
-            <Button className='w-12 h-10 hidden sm:inline ' color='gray' pill>
-                <FaMoon/>
-            </Button>
-            <Link to={'/sign-in'}>
-            <Button gradientDuoTone='purpleToBlue' pill>
-                Sign In
-            </Button>
-            </Link>
-            <Navbar.Toggle/>
-        </div>
-            
-    </Navbar>
-  )
-}
+  const { currentUser } = useSelector((state) => state.user);
 
-export default Header
+ 
+  const path = useLocation().pathname;
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to={`/`}
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Thirteen
+        </span>
+        Blog
+      </Link>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to={"/"}>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to={"/about"}>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to={"/projects"}>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search ..."
+          rightIcon={FaSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <FaSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline " color="gray" pill>
+          <FaMoon />
+        </Button>
+        {currentUser ? (
+          <Dropdown arrowIcon={false} inline label={
+            <Avatar alt="user" img={currentUser.profilePicture} rounded/>
+          }>
+
+            <Dropdown.Header>
+                <span className="block text-sm">@{currentUser.username}</span>
+                <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider/>
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+
+          </Dropdown>
+        ) : (
+          <Link to={"/sign-in"}>
+            <Button gradientDuoTone="purpleToBlue" pill>
+              Sign In
+            </Button>
+          </Link>
+        )}
+        <Navbar.Toggle />
+      </div>
+    </Navbar>
+  );
+};
+
+export default Header;
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,22 +1,32 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username: {
+const userSchema = new mongoose.Schema(
+    {
+      username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      profilePicture: {
+        type: String
+      },
+      isAdmin: {
+        type: Boolean,
+        default: false,
+      },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-    }
-}, {timestamps: true});
-
-const User = mongoose.model('User', userSchema);
-
-export default User;
+    { timestamps: true }
+  );
+  
+  const User = mongoose.model('User', userSchema);
+  
+  export default User;
