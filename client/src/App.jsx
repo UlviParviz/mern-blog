5d562e413681e493abe48d8da9dddfaa55204bdc
Ulvi Spaethe <ulvispaethe@gmail.com>
2024-03-01 18:54:38 +0000

    made dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './layouts/Header'
 import Footer from './layouts/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -18,7 +19,9 @@ const App = () => {
         <Route path='/about' element={<About/>}/>
         <Route path='/sign-in' element={<SignIn/>}/>
         <Route path='/sign-up' element={<SignUp/>} />
+        <Route element={<PrivateRoute/>}>
         <Route path='/dashboard' element={<Dashboard/>}/>
+        </Route>
         <Route path='/projects' element={<Projects/>}/>
       </Routes>
       <Footer/>
