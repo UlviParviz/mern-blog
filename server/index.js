6c19c6966127196663d4c5dbd718552a0b64a4df
Ulvi Spaethe <ulvispaethe@gmail.com>
2024-03-02 23:04:32 +0000

    completed posts server side

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
+import postRoutes from './routes/post.route.js';
 
 const app = express();
 app.use(express.json());
@@ -25,6 +26,7 @@ app.listen(8000, ()=>{
 
 app.use("/api/user", userRoutes)
 app.use("/api/auth", authRoutes)
+app.use('/api/post', postRoutes);
 
 app.use((err, req, res, next) =>{
     const statusCode = err.statusCode || 500;
